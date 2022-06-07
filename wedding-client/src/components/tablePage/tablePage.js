ea45bd4402bc1910320df39e20b3a24664d46bc5
AndreiDruga <75364827+AndreiDruga@users.noreply.github.com>
2022-06-07 19:13:59 +0300

    s

--- wedding-client/src/components/tablePage/tablePage.js
+++ wedding-client/src/components/tablePage/tablePage.js
@@ -1,55 +1,43 @@
 import React from "react";
-import "./searchPage.scss";
-import { InputBase, Paper } from "@mui/material";
-import "@fontsource/alex-brush";
-import "@fontsource/allison";
-import { Divider } from "@mui/material";
-import { Box } from "@mui/system";
+import "./tablePage.scss";
 import { Button } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import data from "../data/wedding.json";
+import { ButtonGroup } from "@mui/material";
+import { Box } from "@mui/system";
+
+
+
 
 const tablePage = () => {
 
-  const handleOnclick = () => {
-    let masa = " ";
-    for (let i in data) {
-      if (data[i].nume === document.getElementById("name").value) {
-        masa += data[i].masa;
-      }
-    }
-    console.log(masa);
-  };
+  const buttons = [
+    <Button key="one">One</Button>,
+    <Button key="two">Two</Button>,
+    <Button key="three">Three</Button>,
+  ];
+
 
   return (
-    <Box className="searchContainer">
-      <h1>Bine ai venit!</h1>
-      <Box className="bottomForm">
-        <Box className="devider">
-          <Divider className="topDevider" />
-        </Box>
-        <h2>nume complet</h2>
-        <Paper
-          className="paper"
-          component="form"
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
-            width: 300,
-            borderRadius: 10,
-          }}
-        >
-          <InputBase sx={{ ml: 1, flex: 1 }} name="name" id="name" />
-          <Divider className="deviderBottom" orientation="vertical" />
-          <Button
-            type="button"
-            onClick={handleOnclick}
-            startIcon={<SearchIcon className="icon" />}
-            sx={{ p: "10px" }}
-          ></Button>
-        </Paper>
-      </Box>
+    <Box className="backGround">
+    <ButtonGroup
+      orientation="vertical"
+      aria-label="vertical outlined button group"
+    >
+      {buttons}
+    </ButtonGroup>
+    <ButtonGroup
+      orientation="vertical"
+      aria-label="vertical contained button group"
+      variant="contained"
+    >
+      {buttons}
+    </ButtonGroup>
+    <ButtonGroup
+      orientation="vertical"
+      aria-label="vertical contained button group"
+      variant="text"
+    >
+      {buttons}
+    </ButtonGroup>
     </Box>
   );
 };
